e185135aa710206ada60e508e67cf521f4d15997
Tikhonov Ruslan <micter_m@mail.ru>
2024-12-01 15:10:49 +0500

    добавленна возможность добавления фото профиля

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os.path
 from pathlib import Path
 
-from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGIN_URL
+from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGIN_URL, MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'user'
+
+MEDIA_URL = '/pictures/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'pictures')
--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as userViews
 from django.contrib.auth import views as authViews
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,7 @@ urlpatterns = [
     path('user/', authViews.LoginView.as_view(template_name='users/user.html'), name="user"),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
