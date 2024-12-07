b9326b023afe3b1491609d03954540359dabe47a
Tikhonov Ruslan <micter_m@mail.ru>
2024-12-07 15:33:20 +0500

    доработано обновление статей, добавлено удаление статей

--- mySite/blog/views.py
+++ mySite/blog/views.py
@@ -3,9 +3,10 @@ from .models import News
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
-                                  UpdateView
+                                  UpdateView,
+                                  DeleteView
                                   )
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # def home(request):
 #     data = {
@@ -50,19 +51,42 @@ class CreateNewsView(LoginRequiredMixin, CreateView):
         ctx['btn_text'] = 'Добавить статью'
         return ctx
 
-class UpdateNewsView(LoginRequiredMixin, UpdateView):
+class UpdateNewsView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = News
     template_name = "blog/create-news.html"
     fields = ['title', 'text']
-    def form_valid(self, form):
-        form.instance.avtor = self.request.user
-        return super().form_valid(form)
+
+
     def get_context_data(self, **kwards):
         ctx = super(UpdateNewsView, self).get_context_data(**kwards)
-
         ctx['title'] = 'Обновление статьи'
         ctx['btn_text'] = 'Обновить статью'
         return ctx
+
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.avtor:
+            return True
+        return False
+
+    def form_valid(self, form):
+        form.instance.avtor = self.request.user
+        return super().form_valid(form)
+
+
+
+
+
+class DeleteNewsView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = News
+    success_url = '/'
+    template_name = 'blog/delete-news.html'
+    def test_func(self):
+        news = self.get_object()
+        if self.request.user == news.avtor:
+            return True
+        return False
+
 
 def contacti(request):
     return render(request, "blog/contacti.html", {"title": "Контакты!"})
